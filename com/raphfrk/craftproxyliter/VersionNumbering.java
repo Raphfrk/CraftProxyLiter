e264d07bb1496499cec51a8da56203f66f2e4570
raphfrk <raphfrk@gmail.com>
2011-05-16 23:17:56 +0100

    Removed debug code about delay through fairness manager

--- com/raphfrk/craftproxyliter/VersionNumbering.java
+++ com/raphfrk/craftproxyliter/VersionNumbering.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "76";
+	final static String version = "77";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
