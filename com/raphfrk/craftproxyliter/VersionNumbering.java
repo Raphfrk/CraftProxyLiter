b0b4b4f6149852e87a8d52feab7811c78c2ab45d
raphfrk <raphfrk@gmail.com>
2011-08-23 23:26:19 +0100

    fixed symlink and added basic build instructions

--- com/raphfrk/craftproxyliter/VersionNumbering.java
+++ com/raphfrk/craftproxyliter/VersionNumbering.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "110";
+	final static String version = "111";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
