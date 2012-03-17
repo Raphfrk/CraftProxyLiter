3df11059825e5f1a7a5b3b313cd8647e9828a6a7
Raphfrk <raphfrk@gmail.com>
2012-03-17 16:18:53 +0000

    1.2.3 compatibility - compression might not work - attempt2

--- com/raphfrk/craftproxyliter/VersionNumbering.java
+++ com/raphfrk/craftproxyliter/VersionNumbering.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "143";
+	final static String version = "144";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
