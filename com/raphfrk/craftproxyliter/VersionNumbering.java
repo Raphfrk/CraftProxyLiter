c233f24e8e517806bc3e11cb798ab3bb684025ac
Raphfrk <raphfrk@gmail.com>
2012-03-18 16:27:18 +0000

    confirming upload

--- com/raphfrk/craftproxyliter/VersionNumbering.java
+++ com/raphfrk/craftproxyliter/VersionNumbering.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class VersionNumbering {
-	final static String version = "144";
+	final static String version = "145";
 
 	static String name = "default";
 	static final String slash = System.getProperty("file.separator");
